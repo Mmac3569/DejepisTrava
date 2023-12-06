125c3a5c92aa94ef6bbd955383b3c6b843f920b3
Matej Maca <macam@arcig.cz>
2023-12-06 19:12:05 +0100

    Test release

---
+++ wwwroot/js/Archive.js
@@ -0,0 +1,20 @@
+﻿
+    var coll = document.getElementsByName("archive");
+    var cont = document.getElementsByName("archive-content-opened");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].classList.add("archive");
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("archive-active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+        content.style.maxHeight = null;
+            } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    }
+    for (i = 0; i < cont.length; i++) {
+        cont[i].style.maxHeight = cont[i].scrollHeight + "px";
+    }
---
+++ wwwroot/js/Collapsible.js
@@ -0,0 +1,20 @@
+﻿
+    var coll = document.getElementsByName("collapsible");
+    var cont = document.getElementsByName("opened");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].classList.add("collapsible");
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+        content.style.maxHeight = null;
+            } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    }
+    for (i = 0; i < cont.length; i++) {
+        cont[i].style.maxHeight = cont[i].scrollHeight + "px";
+    }
